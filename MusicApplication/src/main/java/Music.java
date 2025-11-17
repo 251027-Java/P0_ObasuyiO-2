d703ce183dd984136dafcb97990b9c01abb84f30
“Osi” <oobasuyi@uncc.edu>
2025-11-17 10:27:21 -0600

    completed music class with getters and setters and fields (basis of project)

--- MusicApplication/src/main/java/Music.java
+++ MusicApplication/src/main/java/Music.java
@@ -1,2 +1,64 @@
 public class Music {
+    //fields for music class (song info)
+    private int id;
+    private String title;
+    private String artist;
+    private String album;
+    private boolean saved;
+
+    //constructors for a music object (a song with info essentially)
+    public Music(){}
+
+    public Music(int id, String title, String artist, String album, boolean saved)
+    {
+        this.id = id;
+        this.title = title;
+        this. artist = artist;
+        this.album = album;
+        this.saved = saved;
+    }
+
+    //constructor for a song that is not saved/will not be saved
+    public Music(int id, String title, String artist, String album)
+    {
+        this.id = id;
+        this.title = title;
+        this. artist = artist;
+        this.album = album;
+    }
+
+    //getters and setters
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum(){
+        return album;
+    }
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+
+
+
+
+
 }
--- MusicApplication/src/main/java/Repository/MusicRepo.java
+++ MusicApplication/src/main/java/Repository/MusicRepo.java
@@ -2,3 +2,4 @@ package Repository;
 
 public interface MusicRepo {
 }
+
